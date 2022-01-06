de32c29875aa172da5dd0955b09761d0819f1ea3
Sadaf244 <77284689+Sadaf244@users.noreply.github.com>
2022-01-06 13:30:28 +0530

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -3,6 +3,7 @@ from .models import SchoolUser
 from django.apps import apps
 
 admin.site.register(SchoolUser)
+admin.site.register(Subscription)
 app=apps.get_app_config('graphql_auth')
 for model_name,model in app.models.items():
     admin.site.register(model)
